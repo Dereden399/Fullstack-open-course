1ea725563366893c9b27ac68c23434ce92985bbb
Dereden399 <denisk399@gmail.com>
2021-09-15 00:01:13 +0300

    puhelinluetteloBackend added

--- Osa 3/puhelinluetteloBackend/index.js
+++ Osa 3/puhelinluetteloBackend/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+app.use(express.json())
+
+morgan.token('body', req => JSON.stringify(req.body))
+app.use(morgan('tiny'))
 
 let persons = [
   { id: 1, name: 'Arto Hellas', number: '040-123456' },
@@ -28,6 +33,42 @@ app.get('/api/persons/:id', (req, res) => {
   if(person) res.json(person)
   else res.status(404).end()
 })
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(x => x.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms - :body'), (req, res) => {
+  if(!req.body || !req.body.name || !req.body.number) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+  if(persons.map(x => x.name.toLowerCase()).some(x => x === req.body.name.toLowerCase())) {
+    return res.status(400).json({
+      error: 'name is already in list'
+    })
+  }
+
+  let id = Math.floor(Math.random()*1000)
+  while(persons.map(x => x.id).includes(id)) id = Math.floor(Math.random()*1000)
+  const person = {
+    id: id,
+    name: req.body.name,
+    number: req.body.number
+  }
+  persons = persons.concat(person)
+  res.json(person)
+
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
